d6580756631f40c979eda5ffb431bb4888e6ba6d
Alexander Ryzhov <aryzhov07@gmail.com>
2019-04-05 17:16:28 -0700

    Added comments in utils

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,9 +1,11 @@
 part of mutable_model;
 
+/// Converts an enumeration value to its string representation without the type name
 enumStr(dynamic enumValue) {
   return enumValue?.toString()?.split('.')?.last;
 }
 
+/// Converts a string to an enum value. Works as the inverse of [enumStr()].
 parseEnum(List<dynamic> values, String value) {
   return value == null ? null : values.firstWhere((v) => enumStr(v) == value);
 }
