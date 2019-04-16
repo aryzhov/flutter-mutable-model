3b5ca549f81f13ed6f96e1806559355b73ae8096
Alexander Ryzhov <aryzhov07@gmail.com>
2019-04-15 23:55:55 -0700

    Added MetaModel

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,32 +1,106 @@
 part of mutable_model;
 
-abstract class Model extends ChangeNotifier {
+abstract class MetaModel {
 
+  Map<Property, int> _indexes;
   Iterable<Property> get properties;
+
+  int indexOf(Property p, {bool nullIfAbsent = false}) {
+    if(_indexes == null) {
+      _indexes = Map<Property, int>();
+      int idx = 0;
+      for(var p in properties)
+        _indexes[p] = idx++;
+    }
+    final result = _indexes[p];
+    assert(result != null && !nullIfAbsent, "Property not found");
+    return result;
+  }
+}
+
+abstract class Model<MM extends MetaModel> extends ChangeNotifier {
+
   bool _flushing = false;
-  bool _repeatFlush = false;
+
+  MM get meta;
+
+  List<dynamic> _data;
+  Map<Property, dynamic> _changes;
+
+  List<dynamic> get data {
+    if(_data == null)
+      _data = List.unmodifiable(meta.properties.map((p) => p.store(p.initial)));
+    return _data;
+  }
+
+  bool get changed {
+    return _changes?.isNotEmpty ?? false;
+  }
+
+  Map<Property, dynamic> get changes {
+    return _changes;
+  }
+
+  T operator[]<T>(Property<T> prop) {
+    return prop.load(_getData(prop));
+  }
+
+  dynamic _getData(Property prop) {
+    if(changed && _changes.containsKey(prop))
+      return _changes[prop];
+    return data[meta.indexOf(prop)];
+  }
+
+  void operator []=<T>(Property prop, T value) {
+    _setData(prop, prop.store(value));
+  }
+
+  void _setData(Property prop, dynamic d) {
+    if(prop.dataEquals(d, data[meta.indexOf(prop)])) {
+      if(_changes != null) {
+        _changes.remove(prop);
+        if(_changes.isEmpty)
+          _changes = null;
+      }
+    } else {
+      if(_changes == null)
+        _changes = Map<Property, dynamic>();
+      _changes[prop] = d;
+    }
+  }
+
+  bool isChanged(Property prop) {
+    return _changes != null && _changes.containsKey(prop);
+  }
 
   /// Fires a change event and clears the change flag on all properties. Returns true if there were changes.
   bool flushChanges() {
     if(_flushing) {
-      _repeatFlush = true;
       return false;
     }
     onFlushChanges();
-    final changed = Set.from(properties.where((p) => p.changed));
-    if(changed.isEmpty)
+    if(!changed)
       return false;
+    final ch = Map.from(_changes);
     try {
       _flushing = true;
       notifyListeners();
     } finally {
       _flushing = false;
-      for(var p in changed)
-        p.changed = false;
-      if(_repeatFlush) {
-        _repeatFlush = false;
-        flushChanges();
+      
+      var data2 = List.from(_data);
+      for(var me in ch.entries) {
+        final prop = me.key;
+        final newData = me.value;
+        data2[meta.indexOf(prop)] = newData;
+        if(_changes.containsKey(prop) && prop.dataEquals(_changes[prop], newData))
+          _changes.remove(prop);
       }
+      _data = List.unmodifiable(data2);
+      if(_changes.isNotEmpty)
+        flushChanges();
+      else
+        _changes = null;
     }
     return true;
   }
@@ -36,23 +110,16 @@ abstract class Model extends ChangeNotifier {
   void onFlushChanges() {
   }
 
-  /// Returns true if any of the properties has changed.
-  bool get changed {
-    for(var p in properties)
-      if(p.changed)
-        return true;
-    return false;
-  }
-
   void copyFrom(Model other, {bool clearChanges = true}) {
-    var it0 = other.properties.iterator;
-    var it1 = this.properties.iterator;
-    while(it0.moveNext() && it1.moveNext()) {
-      final p0 = it0.current;
-      final p1 = it1.current;
-      p1.copyFrom(p0);
-      if(clearChanges)
-        p1.changed = false;
+    assert(this.meta == other.meta, "Can only copy models with the same meta class");
+    if(clearChanges) {
+      this._data = other._data;
+      this._changes = other._changes != null ? Map.from(other._changes): null;
+    }
+    else {
+      for(var p in meta.properties) {
+        this._setData(p, other._getData(p));
+      }
     }
   }
 
--- lib/src/properties.dart
+++ lib/src/properties.dart
@@ -2,297 +2,140 @@ part of mutable_model;
 
 /// A property contains a value that can be changed.
 abstract class Property<T> {
-  T value;
-  T get oldValue;
-  bool changed;
-  bool equals(Property<T> other);
-  void copyFrom(Property<T> other);
-  bool get isNull;
-  bool get isNotNull;
-  
-  factory Property([T initialValue]) => SimpleProperty<T>(initialValue);
+  dynamic store(T value) => value;
+  T load(dynamic value) => value as T;
+  bool dataEquals(dynamic a, dynamic b) => a == b;
+  T get initial => null;
 }
 
-/// A property that stores its data without conversion.
-class SimpleProperty<T> implements Property<T> {
+abstract class Prop<T> extends Property<T> {
 
-  @override
-  T value;
+  final T initial;
 
-  @override
-  T oldValue;
-
-  @override
-  bool get changed => !valueEquals(oldValue);
-
-  @override
-  set changed(ch) {
-    if(!ch)
-      oldValue = value;
-  }
-
-  @override
-  bool get isNull => value == null;
-
-  @override
-  bool get isNotNull => value != null;
-
-  SimpleProperty([T initialValue]) {
-    value = initialValue;
-    changed = false;
-  }
-
-  @override
-  bool equals(Property<T> other) {
-    return other is SimpleProperty<T> && valueEquals(other.value);
-  }
-
-  /// Override this method for custom comparison
-  bool valueEquals(T otherValue) {
-    return value == otherValue;
-  }
-
-  void copyFrom(Property<T> other) {
-    value = other.value;
-  }
+  Prop([this.initial]);
 
 }
 
-/// A property that converts its value to an easily serializable form for storing
-abstract class DataProperty<T> implements Property<T> {
-  dynamic _data;
-  dynamic _oldData;
-  bool _changed = false;
 
-  DataProperty([T initialValue]) {
-    this.value = initialValue;
-    changed = false;
-  }
+class StringProp extends Prop<String> {
 
-  @override
-  T get value => dataToValue(data);
-
-  @override
-  set value(T v) {
-    data = valueToData(v);
-  }
-
-  @override
-  T get oldValue => _changed ? dataToValue(_oldData): value;
-
-  @override
-  bool get changed => _changed;
-
-  @override
-  bool get isNull => _data == null;
-
-  @override
-  bool get isNotNull => _data != null;
-
-
-  @override
-  set changed(ch) {
-    _changed = ch;
-    if(!ch) {
-      _oldData = null;
-    }
-  }
-
-  @override
-  bool equals(Property<T> other) {
-    return other is DataProperty<T> && dataEquals(other.data);
-  }
-
-  void copyFrom(Property<T> other) {
-    if(other is DataProperty<T>)
-      data = other.data;
-    else
-      value = other.value;
-  }
-
-  /// Returns the serialized representation of the data
-  dynamic get data => _data;
-
-  /// Sets the value using the serialized representation
-  set data (dynamic d) {
-    if(!dataEquals(d)) {
-      _data = d;
-      if(_changed) {
-        if(dataEquals(_oldData)) {
-          _changed = false;
-          _oldData = null;
-        }
-      } else {
-        _oldData = _data;
-        _changed = true;
-      }
-    }
-  }
-
-  /// Override this method for custom conversion from data to value
-  T dataToValue(dynamic data) {
-    return data as T;
-  }
-
-  /// Override this method for custom conversion value to data
-  dynamic valueToData(T value) {
-    return value;
-  }
-
-  /// Override this method for custom equality behavior
-  bool dataEquals(dynamic newData) {
-    return data == newData;
-  }
+  StringProp([String initial]): super(initial);
 
 }
 
-class StringProp extends DataProperty<String>{
+class BoolProp extends Prop<bool>{
 
-  StringProp([String initialValue]): super(initialValue);
+  BoolProp([bool initial]): super(initial);
 
-}
-
-class BoolProp extends DataProperty<bool>{
-
-  BoolProp([bool initialValue=false]): super(initialValue);
-
-  get value => super.value ?? false;
+  bool load(dynamic value) => value as bool ?? false;
 
 }
 
-class IntProp extends DataProperty<int>{
+class IntProp extends Prop<int>{
 
-  IntProp([int initialValue]): super(initialValue);
+  IntProp([int initial]): super(initial);
 
   @override
-  int dataToValue(dynamic data) {
+  int load(dynamic data) {
     return data is double ? data.toInt(): data;
   }
 
   @override
-  int valueToData(v) {
-    return v is double ? v.toInt(): v;
-  }
-
-  @override
-  bool dataEquals(other) {
-    return data == (other is double ? other.toInt() : other);
+  dynamic store(v) {
+    return v;
   }
 
 }
 
-class DoubleProp extends DataProperty<double>{
+class DoubleProp extends Prop<double>{
 
-  DoubleProp([double initialValue]): super(initialValue);
+  DoubleProp([double initial]): super(initial);
 
   @override
-  double dataToValue(dynamic data) {
+  double load(dynamic data) {
     return data is int ? data.toDouble(): data;
   }
 
-  @override
-  double valueToData(v) {
-    return v is int ? v.toDouble(): v;
-  }
-
-  @override
-  bool dataEquals(other) {
-    return data == (other is int ? other.toDouble() : other);
-  }
-
 }
 
-class EnumProp<E> extends DataProperty<E> {
+class EnumProp<E> extends Prop<E> {
 
-  List<E> enumValues;
+  final List<E> enumValues;
+
+  EnumProp(this.enumValues, [E initial]): super(initial);
+
 
   @override
-  dataToValue(data) => parseEnum(enumValues, data as String);
+  load(data) => parseEnum(enumValues, data as String);
 
   @override
-  valueToData(value) => enumStr(value);
-
-  EnumProp(this.enumValues, [E initialValue]): super(initialValue);
+  store(value) => enumStr(value);
 
 }
 
 /// Stores an int value as a string
-class IntStrProp extends DataProperty<int> {
+class IntStrProp extends Prop<int> {
 
-
-  @override
-  valueToData(i) => i == null ? null : "$i";
+  IntStrProp([int initial]): super(initial);
 
   @override
-  dataToValue(data) => data == null ? null : int.parse(data as String);
+  store(i) => i == null ? null : "$i";
 
   @override
-  bool dataEquals(dynamic newData) {
-    return data == newData;
-  }
-
-  IntStrProp([int initialValue]) {
-    if(initialValue != null)
-      value = initialValue;
-  }
+  load(data) => data == null ? null : int.parse(data as String);
 
 }
 
-class BoolStrProp extends DataProperty<bool> {
+class BoolStrProp extends Prop<bool> {
 
   static final falseStr = "false";
   static final trueStr = "true";
 
-  @override
-  dataToValue(data) => data == null ? null : (data as String).toLowerCase() == trueStr;
+  BoolStrProp([bool initial]): super(initial);
 
   @override
-  valueToData(b) => b == null ? null : b ? trueStr : falseStr;
+  load(data) => data == null ? null : (data as String).toLowerCase() == trueStr;
 
   @override
-  bool dataEquals(dynamic newData) {
-    return data == newData;
-  }
-
-  BoolStrProp([bool initialValue]): super(initialValue);
+  store(b) => b == null ? null : b ? trueStr : falseStr;
 
 }
 
-class DateTimeProp extends DataProperty<DateTime> {
+class DateTimeProp extends Prop<DateTime> {
 
-  DateTimeProp([DateTime initialValue]): super(initialValue);
+  DateTimeProp([DateTime initial]): super(initial);
 
-  @override
-  bool dataEquals(dynamic newData) {
-    return data == newData || ((data is DateTime) && (newData is DateTime) && (data as DateTime).compareTo(newData) == 0);
-  }
 }
 
 /// A base class for properties that store their data as a Map
 /// Provides an equality function that compares the serialized versions of the object
-abstract class MapProp<T> extends DataProperty<T> {
+abstract class MapProp<T> extends Prop<T> {
 
   static const equality = MapEquality();
 
-  bool dataEquals(newData) {
-    if(data is Map && newData is Map)
-        return equality.equals(data, newData);
+  MapProp([T initial]): super(initial);
+
+  bool dataEquals(a, b) {
+    if(a is Map && b is Map)
+        return equality.equals(a, b);
     else
-      return data == newData;
+      return a == b;
   }
 
 }
 
 /// A property that stores its data as a list.
-class ListProp<T> extends DataProperty<List<T>> {
+class ListProp<T> extends Prop<List<T>> {
 
   static const equality = ListEquality();
 
-  bool dataEquals(newData) {
-    if(data is List && newData is List)
-        return equality.equals(data, newData);
+  ListProp(List<T> initial): super(initial);
+
+  bool dataEquals(a, b) {
+    if(a is List && b is List)
+        return equality.equals(a, b);
     else
-      return data == newData;
+      return a == b;
   }
 
 }
